197fcb543fe661695278665e22998031adccab31
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-21 12:36:20 +0530

    Video 11 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,27 +1,85 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleUpClick = ()=> {
+  // toUpperCase
+  const handleUpClick = () => {
     //  console.log("Uppercase was clicked" + text);
-     let newText = text.toUpperCase();
-     setText(newText);
-    }
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event)=> {
-        // console.log("On Change");
-        setText(event.target.value);
-       }
-    
-const [text, setText] = useState('Enter text here');
-// text = "new text"; //  Wrong way to change the state
-// setText("new text"); // Correct way to change the state
+  // toLowerCase
+  const handleLoClick = () => {
+    //  console.log("Lowercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  // Clear Text
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Change");
+    setText(event.target.value);
+  };
+
+  //  Copy Text
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  // Remove Extra Spaces
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const [text, setText] = useState("");
+  // text = "new text"; //  Wrong way to change the state
+  // setText("new text"); // Correct way to change the state
   return (
-    <div>
+    <>
+      <div className="container">
         <h1>{props.heading}</h1>
-    <div className="mb-3">
-       <textarea className="form-control" value= {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-    </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
-  )
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
