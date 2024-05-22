a26e55e781c8e6f9718103086d88f13be5d82800
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-05-22 16:50:12 +0530

    Video 16 Completed

--- src/App.js
+++ src/App.js
@@ -25,11 +25,20 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
     }
 
   }
@@ -41,7 +50,7 @@ function App() {
       <Alert alert={alert}/>
       <div className="container my-3">
       <TextForm showAlert={showAlert} heading= "Enter the text to analyze below"  mode={mode}/>
-      {/* <About/> */}
+       {/* <About/>  */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,6 +37,26 @@ export default function TextForm(props) {
     props.showAlert("Copied to Clipboard!", "success");
   };
 
+  //  Text Bold
+  const handleTextBold = () => {
+    setIsBold(!isBold);
+  };
+
+  //  Text Italic
+  const handleTextItalic = () => {
+    setIsItalic(!isItalic);
+  };
+
+  // Text Strikethrough
+  const handleTextStrikethrough = () => {
+    setIsStrikethrough(!isStrikethrough);
+  };
+
+  // Text Underline
+  const handleTextUnderline = () => {
+    setIsUnderline(!isUnderline);
+  };
+
   // Remove Extra Spaces
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
@@ -45,45 +65,102 @@ export default function TextForm(props) {
   };
 
   const [text, setText] = useState("");
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isStrikethrough, setIsStrikethrough] = useState(false);
+  const [isUnderline, setIsUnderline] = useState(false);
   // text = "new text"; //  Wrong way to change the state
   // setText("new text"); // Correct way to change the state
   return (
     <>
-      <div className="container"  style={{color: props.mode==='dark'?'white':'#042743'}}>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control"
             value={text}
-            onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'gray':'white', color: props.mode==='dark'?'white':'#042743'}}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "gray" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+              fontWeight: isBold ? "bold" : "normal",
+              fontStyle: isItalic ? "italic" : "normal",
+              textDecoration: isStrikethrough ? "line-through" : "none",
+              textDecoration: isUnderline ? "underline" : "none",
+            }}
             id="myBox"
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+        <button className="btn btn-secondary mx-1 my-1" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+        <button className="btn btn-secondary mx-1 my-1" onClick={handleLoClick}>
           Convert to Lowercase
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleClearClick}
+        >
           Clear Text
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+        <button className="btn btn-secondary mx-1 my-1" onClick={handleCopy}>
           Copy Text
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleTextBold}
+        >
+          {isBold ? "Convert to Normal" : "Convert to Bold"}
+        </button>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleTextItalic}
+        >
+          {isItalic ? "Convert to Normal" : "Convert to Italic"}
+        </button>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleTextStrikethrough}
+        >
+          {isStrikethrough ? "Convert to Normal" : "Convert to Strikethrough"}
+        </button>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleTextUnderline}
+        >
+          {isUnderline ? "Convert to Normal" : "Convert to Underline"}
+        </button>
+        <button
+          className="btn btn-secondary mx-1 my-1"
+          onClick={handleExtraSpaces}
+        >
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h2>Your text summary</h2>
         <p>
-          {text.split(" ").length} words and {text.length} characters
+          {
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </p>
       </div>
     </>
   );
